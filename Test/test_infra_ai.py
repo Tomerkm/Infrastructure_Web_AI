529c414acd76f33f4fb8207a36e00d8d3cc239e0
Tomerkm <tomerkeiynan@gmail.com>
2025-06-27 23:33:06 +0300

    Implemented Test For Aluminum And Browse Use And Pacakges In a file

--- Test/test_infra_ai.py
+++ Test/test_infra_ai.py
@@ -1,4 +1,49 @@
+# All Fixtures Are Found In conftest.py file
+import asyncio
 import os
 
-def test_web_browser_and_alumnium(convert_action_to_list_tuple, set_api_key, al, load_global_var, driver):
-    assert 1 == 1
+import allure
+
+
+async def browser_use_run(browser_use_agent):
+    history = await browser_use_agent.run()
+    result = history.final_result()
+
+    assert result, f"Result is {result}"
+
+
+def alumnium_run(convert_action_to_list_tuple, al):
+    commands_mapping = {"do": al.do,
+                        "check": al.check,
+                        "get": al.get
+                        }
+
+    for command_al_info in convert_action_to_list_tuple:
+
+        data_len = len(command_al_info)
+        if data_len < 2:
+            print(f"Data Len Should be 2 or 3 and not {data_len}")
+            continue
+
+        op_command = command_al_info[0].lower().strip()
+
+        if not (op_command in ["do", "check", "get"]):
+            print(f"OP Command need be one of the values: do, check, get and not {op_command}")
+            continue
+
+        text_command = command_al_info[1].strip()
+
+        if "get" == op_command:
+            expected_result = command_al_info[2].strip()
+            data_result = commands_mapping[op_command](text_command)
+            assert data_result == expected_result, f"Got Result = {data_result}, Expected Result = {expected_result}"
+
+        else:
+            commands_mapping[op_command](text_command)
+@allure.title("Test Web AI")
+@allure.description("This Test Tests Alumnium And Browser Use")
+def test_web_browser_and_alumnium(convert_action_to_list_tuple, set_api_key, al, driver, browser_use_agent):
+    if os.environ["FRAMEWORK"].lower().strip() == "Alumnium".lower():
+        alumnium_run(convert_action_to_list_tuple=convert_action_to_list_tuple, al=al)
+    elif os.environ["FRAMEWORK"].lower().replace(" ", "") == "Browser Use".lower().replace(" ", ""):
+        asyncio.run(browser_use_run(browser_use_agent=browser_use_agent))
--- conftest.py
+++ conftest.py
@@ -1,10 +1,15 @@
+from dotenv import load_dotenv
+# Order is matter, Alumni call constructor and init the model to the default openapi.
+# So I load env before it happening
+load_dotenv()
+
 import os
 import ast
 from selenium.webdriver import Chrome, Firefox, Edge, Safari
-
+from browser_use import Agent
+from browser_use.llm import ChatOpenAI
 from alumnium import Alumni
 from pytest import fixture
-from dotenv import load_dotenv
 
 # Use For Mapping To The Correct Browser According User Input
 gb_browsers_storage = {"chrome": Chrome,
@@ -18,27 +23,34 @@ gb_api_keys_mapping_storage = {
     "anthropic": "ANTHROPIC_API_KEY",
     "google": "GOOGLE_API_KEY",
     "openai": "OPENAI_API_KEY",
-    "ollama": ""
+    "ollama": "OLLAMA_API_KEY",
+    "azure": "AZURE_OPENAI_KEY",
+    "deepseek": "DEEPSEEK_API_KEY",
+    "grok": "GROK_API_KEY",
+    "novita": "NOVITA_API_KEY"
 }
 
 
 # Use To Read Environment Variables From .env File
-@fixture(scope="session", autouse=True)
-def load_global_var():
-    load_dotenv()
 
 
 # Convert The String To List Of Tuples - More Comfortable
 @fixture(scope="session", autouse=True)
-def convert_action_to_list_tuple(load_global_var):
-    return ast.literal_eval(os.environ["ACTION"])
+def convert_action_to_list_tuple():
+    # Only Alummnium Support This Format
+    if os.environ["FRAMEWORK"].lower().strip() != "Alumnium".lower():
+        return None
+    else:
+        return ast.literal_eval(os.environ["ACTION"])
 
 
 @fixture(scope="session", autouse=True)
-def set_api_key(load_global_var):
+def set_api_key():
     global gb_api_keys_mapping_storage
 
-    model = os.environ["MODEL_NAME"].lower().strip()
+    model = os.environ["ALUMNIUM_MODEL"].lower().strip()
+    if "ollama" in model or model in "ollama":
+        return None
 
     # If User Insert model name with version, it will find the model name only for mapping
     for key in gb_api_keys_mapping_storage.keys():
@@ -53,18 +65,42 @@ def set_api_key(load_global_var):
 
 # Init Browser And Get URL To Start Test It
 @fixture(scope="session", autouse=True)
-def driver(load_global_var):
-    global gb_browsers_storage
+def driver():
+    # Only Alummnium Use Driver - Selenium
+    if os.environ["FRAMEWORK"].lower().strip() != "Alumnium".lower():
+        yield None
+    else:
+        global gb_browsers_storage
 
-    driver = gb_browsers_storage[os.environ["BROWSER"].lower().strip()]()
-    driver.get(os.environ["URL"])
-    yield driver
-    driver.quit()
+        driver_res = gb_browsers_storage[os.environ["BROWSER"].lower().strip()]()
+        driver_res.get(os.environ["URL"])
+        yield driver_res
+        driver_res.quit()
 
 
 # Get Alumni
 @fixture(scope="session", autouse=True)
-def al(load_global_var, driver, convert_action_to_list_tuple):
-    al = Alumni(driver)
-    yield al
-    al.quit()
+def al(driver, set_api_key, convert_action_to_list_tuple):
+    # Only Alummnium Use Driver - Selenium
+    if os.environ["FRAMEWORK"].lower().strip() != "Alumnium".lower():
+        yield None
+    else:
+        al_res = Alumni(driver)
+        yield al_res
+        al_res.quit()
+
+
+@fixture(scope="session", autouse=True)
+def browser_use_agent(set_api_key):
+    # Only Relevant To Browser Use
+    if os.environ["FRAMEWORK"].lower().replace(" ", "") != "Browser Use".lower().replace(" ", ""):
+        yield None
+    else:
+        url = os.environ["URL"]
+        action = os.environ["ACTION"]
+
+        agent = Agent(
+            task=f"Go To {url} Website, And {action}",
+            llm=ChatOpenAI(model=os.environ["ALUMNIUM_MODEL"].lower().strip()),
+        )
+        yield agent
